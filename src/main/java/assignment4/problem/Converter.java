bd724478f2327909aeb130f3a1c82f7fb7a6530f
Vahid Alizadeh <vahid.alizadeh@gmail.com>
2021-11-01 15:32:58 -0500

    HW4

---
+++ src/main/java/assignment4/problem/Converter.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021.
+ * Vahid Alizadeh
+ * SE 450
+ * DePaul University
+ */
+
+package assignment4.problem;
+
+import assignment4.solution.CoR.MileHandler;
+import assignment4.solution.CoRDecorator.AddUnitDecorator;
+import assignment4.solution.CoRDecorator.ExpDecorator;
+import assignment4.solution.CoRDecorator.RoundDecorator;
+
+public class Converter {
+  
+	
+	public static void main(String[] args) {
+    	ConverterGUI ui = new ConverterGUI();
+    	
+    }
+	
+	//You can add method (s) if necessary here or in a separate class to implement the converter logic using the patterns
+		//static Convert(..) method?
+		// Instantiating Handlers and setSuccessors?
+		//Decorators?
+
+}
